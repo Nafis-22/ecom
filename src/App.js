af3d93c530c2b0ff59713d1a6a300eff3eb569f9
Nafis-22 <nafisabegum349@gmail.com>
2022-09-03 17:12:28 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import logo from './logo.svg';
-import Home from "./componant/Home"
-import './App.css';
-import Cart from './componant/Cart';
+import logo from "./logo.svg";
+import Home from "./componant/Home";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Navbar from "./componant/Navbar";
+import Products from "./componant/Products";
+import Cart from "./componant/Cart";
 
 function App() {
   return (
     <>
-      Main
-      <Home/>
-      {/* <Cart/> */}
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products" component={Products} />
+        <Route exact path="/cart" component={Cart} />
+      </Switch>
     </>
+
+    // <Home />
   );
 }
 
--- src/componant/Products.js
+++ src/componant/Products.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from "react";
 import Cart from "./Cart";
-import {useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 
 function Products() {
   const [cart, setCart] = useState([]);
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState(products);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
@@ -32,9 +34,92 @@ function Products() {
   //     cnt++;
   //   }
   // }
+  const Loading = () => {
+    return <>Loading...</>;
+  };
+
+  const filterProduct = (cat) => {
+    const updatedList = products.filter((x) => x.category == cat);
+    setFilter(updatedList);
+  };
+
+  const ShowProducts = () => {
+    return (
+      <>
+        <div className="buttons d-flex justify-content-center mb-5 pb-5  ">
+          <buttons
+            className="btn btn-outline-dark me-2"
+            onClick={() => setFilter(products)}
+          >
+            All
+          </buttons>
+          <buttons
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("men's clothing")}
+          >
+            Men's Clothing
+          </buttons>
+          <buttons
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("women's clothing")}
+          >
+            Women's Clothing
+          </buttons>
+          <buttons
+            className="btn btn-outline-dark me-2 "
+            onClick={() => filterProduct("jewelery")}
+          >
+            Jewelerys
+          </buttons>
+          <buttons
+            className="btn btn-outline-dark me-2"
+            onClick={() => filterProduct("electronics")}
+          >
+            Electronic
+          </buttons>
+        </div>
+        {products.map((v, i) => {
+          return (
+            <>
+              <div className="col-md-3 mb-4  ">
+                <div className="card h-100 text-center p-4" key={v.id}>
+                  <img
+                    src={v.image}
+                    className="card-img-top"
+                    alt={v.title}
+                    height="250px"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title mb-0">
+                      {v.title.substring(0, 12)}...
+                    </h5>
+                    <p className="card-text lad fw-bold">${v.price}</p>
+
+                    <button
+                      className="btn btn-outline-dark mx-2"
+                      onClick={() => addToCart(i)}
+                    >
+                      ADD TO CART
+                    </button>
+                    {/* <NavLink
+                      to={`/products/${product.id}`}
+                      className="btn btn-outline-dark"
+                    >
+                      View
+                    </NavLink> */}
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        })}
+      </>
+    );
+  };
+
   return (
     <>
-      <div className="groceries">
+      {/* <div className="groceries">
         {products.map((v, i) => (
           <div>
             <img src={v.image} alt="A vegetable." height="150" width="150" />
@@ -42,17 +127,23 @@ function Products() {
             <button onClick={() => addToCart(i)}>Add to Cart</button>
           </div>
         ))}
+      </div> */}
+      <div className="containr my-5 py-5 mx-3 my-3">
+        <div className="row">
+          <div className="col-12 mb-5">
+            <h1 className="dispaly-6 fw-bolder text-center">Latest Products</h1>
+            <hr />
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          {loading ? <Loading /> : <ShowProducts />}
+        </div>
       </div>
 
-      
-    <div>Products</div>
-    Hi cart 
-    {/* <div>Items: {cnt}</div> */}
+      {/* <div>Items: {cnt}</div> */}
       <Cart item={cart} />
-    
     </>
-    
-  )
+  );
 }
 
-export default Products
+export default Products;
